336aa778c725a0d0da842ac5077a036e393e8628
iq0x <88105363+iq0x@users.noreply.github.com>
2021-08-18 09:07:17 +0200

    Add files via upload

---
+++ dmtoeuro.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    double dm;
+    double result;
+    std::cout << "DM to EURO Converter\n";
+    std::cout << "DM:";
+    std::cin >> dm;
+    result = dm * 1.95583;
+    std::cout << result;
+    std::cout << " Euro";
+    return 0;
+}
