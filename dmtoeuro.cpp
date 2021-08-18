5e866afc9a55fec97671d8564e1f33b1d21d4d93
iq0x <88105363+iq0x@users.noreply.github.com>
2021-08-19 00:35:21 +0200

    Update dmtoeuro.cpp

--- dmtoeuro.cpp
+++ dmtoeuro.cpp
@@ -3,11 +3,16 @@
 int main() {
     double dm;
     double result;
+    
     std::cout << "DM to EURO Converter\n";
     std::cout << "DM:";
+    
     std::cin >> dm;
+    
     result = dm * 1.95583;
+    
     std::cout << result;
     std::cout << " Euro";
+    
     return 0;
 }
